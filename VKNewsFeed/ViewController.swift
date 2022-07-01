ad5dc397760c0ac9c4b270525774a0f1d52fbf19
Slava <Slava7711240@gmail.com>
2022-07-02 01:15:28 +0300

    Installation in Carthage

---
+++ VKNewsFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKNewsFeed
+//
+//  Created by Slava Orlov on 02.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
